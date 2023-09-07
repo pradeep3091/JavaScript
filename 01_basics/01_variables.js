f2032a49ab5f199dea439ec5850899f70035033a
pradeep3091 <61347523+pradeep3091@users.noreply.github.com>
2023-09-07 10:41:54 +0000

    vaiables and datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=10001
+let accountEmail="pradeep66@gmail.com"
+var accountpassword="256355"
+accountCity="jaipur"
+let  state
+
+// accountId=302//clear
+accountEmail="prade66@gmail.com"
+accountpassword="2563"
+accountEmail="Goa"
+console.log(accountId);
+console.log(accountpassword);
+console.log(accountEmail);
+console.log(state)
+
+// block scope and functional scope
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";///treat all js code newer version
+
+console.log(3+3);
+
+// Number
+// BigInt
+// String
+// Boolean
+//null=>standalone value
+//undefined=>
+//symbol =>unique
+
+
+//object
+
+console.log(typeof null)
+
+console.log(typeof undefined)
+
+
