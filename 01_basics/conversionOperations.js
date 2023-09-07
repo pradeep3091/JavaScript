573b9ea4f2179925114f48c0fd16b4a6e5136fec
pradeep3091 <61347523+pradeep3091@users.noreply.github.com>
2023-09-07 11:04:16 +0000

    conversion

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,22 @@
+let score="3a3"
+
+console.log(typeof score);
+
+//conversion
+let  scoreNumber=Number(score)
+
+console.log( typeof scoreNumber)
+console.log(scoreNumber)  //NaN- not a number
+
+
+let isLoggedIn = 'hi'
+
+let BoolIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(BoolIsLoggedIn)
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
